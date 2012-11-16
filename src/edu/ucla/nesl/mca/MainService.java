d4e679f0ba2b9fd2225f95ccb8465238fc7fac31
chenguangshen <pscggeforce@gmail.com>
2012-11-16 13:35:50 -0800

    new file:   proguard-project.txt 	new file:   project.properties 	new file:   src/edu/ucla/nesl/mca/MainActivity.java 	new file:   src/edu/ucla/nesl/mca/MainService.java

---
+++ src/edu/ucla/nesl/mca/MainService.java
@@ -0,0 +1,20 @@
+package edu.ucla.nesl.mca;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MainService extends Service {
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+    public void onCreate() {
+		
+	}
+
+}
