658ffa03c3e042042e1512abbe71be0ccfdf7011
chenguangshen <pscggeforce@gmail.com>
2012-11-26 02:19:24 -0800

    modified:   gen/edu/ucla/nesl/mca/R.java 	modified:   testFiles/JSON-DrivingSafety.txt 	modified:   testFiles/JSON-TransportationMode.txt 	new file:   testFiles/JSON_Test.txt 	modified:   .classpath 	modified:   .gitignore 	modified:   res/layout/activity_main.xml 	modified:   res/values/strings.xml

--- gen/edu/ucla/nesl/mca/R.java
+++ gen/edu/ucla/nesl/mca/R.java
@@ -15,9 +15,10 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int button1=0x7f070001;
-        public static final int menu_settings=0x7f070002;
-        public static final int textView1=0x7f070000;
+        public static final int button1=0x7f070002;
+        public static final int classifierInfo=0x7f070001;
+        public static final int debugInfoText=0x7f070000;
+        public static final int menu_settings=0x7f070003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -29,6 +30,7 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int button=0x7f040004;
         public static final int hello_world=0x7f040001;
+        public static final int large_text=0x7f040005;
         public static final int menu_settings=0x7f040002;
         public static final int title_activity_main=0x7f040003;
     }
